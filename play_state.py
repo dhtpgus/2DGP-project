ee94c32ad2a736f6b4d0601656bc197927b8781c
dhtpgus <dhtpgus1414@gmail.com>
2022-10-19 17:58:02 +0900

    enemy 임시

--- play_state.py
+++ play_state.py
@@ -13,6 +13,31 @@ class Map:
         self.image.draw(1280 // 2, 720 // 2)
 
 
+sense_range = 0
+enemy_attack_range = 0
+enemy_attack = False
+
+# 움직이는 적 객체 생성, 적은 플레이어(skul.x,skul.y)가 sense_range안에 들어오면 플레이어가 range 안에있는동안 플레이어를 일정속도로 따라다닌다.
+# 그리고 attack_range범위안에 플레이어가 들어오면 플레이어를 공격한다.
+class enemy:  # 잡몹은 y방향 이동없음
+    def __init__(self):
+        self.x, self.y = 800, 200
+        self.frame = 0
+        self.dirx = 0
+        # self.image=load_image('enemy.png')
+        self.item = None
+
+    def update(self):
+        global sense_range, enemy_attack
+        self.frame = (self.frame + 1) % 8
+        self.x += self.dirx * 1
+        if enemy_attack:
+
+
+    def draw(self):
+        self.image.clip_draw((self.frame // 2) * 77, 60, 72, 60, self.x, self.y - 2, 66, 55)
+
+
 class Skul:
     def __init__(self):
         global attackR_images
@@ -57,7 +82,6 @@ class Skul:
             if self.y <= 200:  # 나중에 현재위치에 맞게 바꿔줘야함
                 self.diry = 0
                 falling = False
-            print(self.y)
         self.y += self.diry * 1
         delay(0.025)
 
